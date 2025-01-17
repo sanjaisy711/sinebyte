939ef658c172b350651695aa04e884717fbc2b34
sanjaisy711 <sanjaip711@gmail.com>
2025-01-17 17:50:25 +0530

    comment

--- src/app/page.js
+++ src/app/page.js
@@ -279,7 +279,7 @@ export default function Page() {
 
               <div className="mt-4">
                 <Link
-                  href="page-aboutus.html"
+                  href="#"
                   className="relative inline-flex items-center font-semibold tracking-wide align-middle text-base text-center border-none after:content-[''] after:absolute after:h-px after:w-0 hover:after:w-full after:end-0 hover:after:end-auto after:bottom-0 after:start-0 after:duration-500 text-indigo-600 hover:text-indigo-600 after:bg-indigo-600 duration-500 ease-in-out"
                 >
                   Find Out More{" "}
