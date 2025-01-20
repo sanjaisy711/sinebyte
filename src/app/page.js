3b13fee85f9fd545e0a66b941d5d466718621be8
sanjaisy711 <sanjaip711@gmail.com>
2025-01-20 18:11:37 +0530

    comment

--- src/app/page.js
+++ src/app/page.js
@@ -416,7 +416,7 @@ export default function Page() {
             <div className="relative order-1 md:order-2">
               <div className="relative overflow-hidden rounded-lg border border-indigo-600/40 bg-indigo-600/10 dark:bg-indigo-600/20 lg:pe-12 pe-8 lg:pt-12 pt-8 lg:ms-8">
                 <Image
-                  src="/images/classic05.png"
+                  src="/images/workflow.PNG"
                   width={0}
                   height={0}
                   sizes="100vw"
