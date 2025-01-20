58813f420444b5f513582e4a88b6e042fb8162c3
sanjaisy711 <sanjaip711@gmail.com>
2025-01-20 12:02:30 +0530

    comment

--- src/app/page.js
+++ src/app/page.js
@@ -294,12 +294,15 @@ export default function Page() {
           <div className="grid md:grid-cols-2 grid-cols-1 items-center gap-[30px]">
             <div className="relative order-1 md:order-2">
               <div className="relative overflow-hidden rounded-lg border border-indigo-600/40 bg-indigo-600/10 dark:bg-indigo-600/20 lg:pe-12 pe-8 lg:pt-12 pt-8 lg:ms-8">
-                <img
+                <video autoPlay muted loop>
+                  <source src="/images/processS.mp4" type="video/mp4" />
+                </video>
+                {/* <img
                   src="/images/process.gif"
                   alt="loading..."
                   style={{ width: "100%", height: "auto" }}
                   className="ltr:rounded-tr-lg rtl:rounded-tl-lg"
-                />
+                /> */}
                 {/* <img
                   src="/images/process.gif"
                   alt="Description of the image"
