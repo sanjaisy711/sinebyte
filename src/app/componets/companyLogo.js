b8ce2e8e3e3de2a45f82b31ba395b2fdf45a1983
sanjaisy711 <sanjaip711@gmail.com>
2025-01-20 14:49:25 +0530

    comment

--- src/app/componets/companyLogo.js
+++ src/app/componets/companyLogo.js
@@ -1,32 +1,68 @@
-import React from 'react'
-import Image  from 'next/image'
+import React from "react";
+import Image from "next/image";
 
-export default function CompanyLogo(){
-    return(
-        <div className="grid md:grid-cols-6 grid-cols-2 justify-center gap-[30px]">
-            <div className="mx-auto py-4">
-              <Image src="/images/client/amazon.svg" width={72} height={24} className="h-6" alt="" />
-            </div>
+export default function CompanyLogo() {
+  return (
+    <div className="grid md:grid-cols-6 grid-cols-2 justify-center gap-[30px]">
+      <div className="mx-auto py-4">
+        <Image
+          src="/images/client/amazon.svg"
+          width={72}
+          height={24}
+          className="h-6"
+          alt="amazon"
+        />
+      </div>
 
-            <div className="mx-auto py-4">
-              <Image src="/images/client/google.svg" width={72} height={24} className="h-6" alt="" />
-            </div>
+      <div className="mx-auto py-4">
+        <Image
+          src="/images/client/google.svg"
+          width={72}
+          height={24}
+          className="h-6"
+          alt="google"
+        />
+      </div>
 
-            <div className="mx-auto py-4">
-              <Image src="/images/client/lenovo.svg" width={72} height={24} className="h-6" alt="" />
-            </div>
+      <div className="mx-auto py-4">
+        <Image
+          src="/images/client/lenovo.svg"
+          width={72}
+          height={24}
+          className="h-6"
+          alt="lenovo"
+        />
+      </div>
 
-            <div className="mx-auto py-4">
-              <Image src="/images/client/paypal.svg" width={72} height={24} className="h-6" alt="" />
-            </div>
+      <div className="mx-auto py-4">
+        <Image
+          src="/images/client/paypal.svg"
+          width={72}
+          height={24}
+          className="h-6"
+          alt="paypal"
+        />
+      </div>
 
-            <div className="mx-auto py-4">
-              <Image src="/images/client/spotify.svg" width={72} height={24} className="h-6" alt="" />
-            </div>
+      <div className="mx-auto py-4">
+        <Image
+          src="/images/client/spotify.svg"
+          width={72}
+          height={24}
+          className="h-6"
+          alt="spotify"
+        />
+      </div>
 
-            <div className="mx-auto py-4">
-              <Image src="/images/client/shopify.svg" width={72} height={24} className="h-6" alt="" />
-            </div>
-        </div>
-    )
+      <div className="mx-auto py-4">
+        <Image
+          src="/images/client/shopify.svg"
+          width={72}
+          height={24}
+          className="h-6"
+          alt="shopify"
+        />
+      </div>
+    </div>
+  );
 }
